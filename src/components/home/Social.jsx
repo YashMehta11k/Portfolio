227c968c4895519af504528042368861eea72d80
YashMehta11k <yashmht44@gmail.com>
2024-07-03 02:01:10 +0200

    Home component created with resposiveness

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social=()=>{
+    return(
+        <div className="home__social">
+            <a href="https://www.instagram.com/_yashmehta18_/" className="home__social-icon" target="_blank">
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/yashmehta11k/" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/YashMehta11k" className="home__social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
